1aec91e096a3bf4e0cd358625812a7f88c63c27b
Akashmahto022 <akashmahto1169@gmail.com>
2024-11-01 18:11:24 +0530

    created a page for the blog list and the blog create and edit

--- blogapp/blog/urls.py
+++ blogapp/blog/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('all-blog/', views.blog_list, name = 'blog_list'),
+    path('blog-create/', views.blog_create, name = 'blog_create'),
+    path('<int:blog_id>/blog-edit/', views.blog_edit, name = 'blog_edit'),
+    path('<int:blog_id>/blog-delete-confirmation', views.blog_delete, name = 'blog_delete'),
 ]
