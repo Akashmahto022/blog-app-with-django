7ddc30377317db2024493642c8ae71e7f86e67c1
Akashmahto022 <akashmahto1169@gmail.com>
2024-11-02 08:39:30 +0530

    create a form for creating blog

--- blogapp/blog/models.py
+++ blogapp/blog/models.py
@@ -8,7 +8,7 @@ class Blog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.TextField(max_length=120)
     description = models.TextField(max_length=6000)
-    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    photo = models.ImageField(upload_to='photos/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
